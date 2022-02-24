f09b0bb97e1b48e2c2b0e087f1c4dc6c2f58f816
MatheusMachad00 <matheus_m_machado@hotmail.com>
2022-02-24 15:21:27 -0300

    HTML and CSS finished

---
+++ script/scrpit.js
@@ -0,0 +1,4 @@
+function validURL(url) {
+    var regexExpression = /^(ftp|http|https|chrome|:\/\/|\.|@){2,}(localhost|\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|\S*:\w*@)*([a-zA-Z]|(\d{1,3}|\.){7}){1,}(\w|\.{2,}|\.[a-zA-Z]{2,3}|\/|\?|&|:\d|@|=|\/|\(.*\)|#|-|%)*$/gum
+    return regexExpression.test(url);
+}
