712e07bdd7d98be28944fd6a0a6bb6525fe31f2a
MatheusMachad00 <matheus_m_machado@hotmail.com>
2022-02-26 11:38:29 -0300

    Axios post function finished

--- script/scrpit.js
+++ script/scrpit.js
@@ -4,13 +4,19 @@ let obj = {
     model: '',
 	neck: '',
 	material: '',
-	image: '',
+	image: '', //https://wallpaperaccess.com/full/4817951.jpg para testes
 	owner: '',
 	author: ''
 }
 let o1, o2, o3;
 
-/* outras funções */
+function yourName (){
+    let name = prompt("Por favor, digite seu nome: ");
+    obj.author = name;
+    obj.owner = name;
+}
+
+window.onload = yourName;
 
 function changeBorderOption1(option1, type) {
     const typeSelected = document.querySelector(".options1 .borderColor");
@@ -21,8 +27,6 @@ function changeBorderOption1(option1, type) {
     obj.model = type;
     o1 = true;
     turnOnButton();
-    /* console.log(obj.model);
-    console.log(type); */
 }
 
 function changeBorderOption2(option2, collar) {
@@ -34,8 +38,6 @@ function changeBorderOption2(option2, collar) {
     obj.neck = collar;
     o2 = true;
     turnOnButton();
-    /* console.log(obj.neck);
-    console.log(collar); */
 }
 
 function changeBorderOption3(option3, material) {
@@ -47,23 +49,57 @@ function changeBorderOption3(option3, material) {
     obj.material = material;
     o3 = true;
     turnOnButton();
-    /* console.log(obj.material);
-    console.log(material); */
 }
 
+//document.getElementById("shirtIMG").onchange = () => {console.log("aa")}; //onchange -> pesquisar
+
+//let img = document.querySelector("input").value;
+
 function turnOnButton (){
-    const buttonOn = document.querySelector("button");
-    if (o1 === true && o2 === true && o3 === true){
+    const buttonOn = document.getElementById("myBtn");
+    if ((o1 && o2  && o3 ) && validURL(document.getElementById("shirtIMG").value)){    // && validURL(document.getElementById("shirtIMG").value)
+        //console.log(validURL(document.getElementById("shirtIMG")));
         buttonOn.disabled = false;
         buttonOn.classList.add("finalizeOrder");
+        obj.image = document.querySelector("input").value;
     } else{
         buttonOn.disabled = true;
         buttonOn.classList.remove("finalizeOrder");
     }
 }
 
-
 function validURL(url) {
+    console.log(url);
+    if (url.length < 1) return false;
+
     var regexExpression = /^(ftp|http|https|chrome|:\/\/|\.|@){2,}(localhost|\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|\S*:\w*@)*([a-zA-Z]|(\d{1,3}|\.){7}){1,}(\w|\.{2,}|\.[a-zA-Z]{2,3}|\/|\?|&|:\d|@|=|\/|\(.*\)|#|-|%)*$/gum
     return regexExpression.test(url);
+}
+
+function sendOrder() {
+    const promise = axios.post(`${LINK_API}`, obj);
+    promise.then(orderOk);
+    promise.catch(orderNotOk);
+}
+
+function orderOk (response) {
+    alert("Seu pedido foi confirmado! :)");
+    createHTML ();
+    console.log(response);
+}
+
+function orderNotOk (response) {
+    alert("Ops, não conseguimos processar sua encomenda. :(");
+    console.log(response);
+    console.log(obj);
+}
+
+function createdOrderHTML (){
+    const lastOrders = document.querySelector(".containerFooter");
+    lastOrders.innerHTML += "";
+    lastOrders.innerHTML += `<div class="lastOrders">
+    <img src="${obj.image}" alt="${obj.image}">
+    <p><strong>Criador: </strong>${obj.author}</p>
+</div>
+    `;
 }
