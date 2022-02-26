bcbcb276087f27302d935b13d35b41360afc9705
MatheusMachad00 <matheus_m_machado@hotmail.com>
2022-02-26 15:08:49 -0300

    Axios get function created

--- script/scrpit.js
+++ script/scrpit.js
@@ -2,25 +2,29 @@
 const LINK_API = "https://mock-api.driven.com.br/api/v4/shirts-api/shirts";
 let obj = {
     model: '',
-	neck: '',
-	material: '',
-	image: '', //https://wallpaperaccess.com/full/4817951.jpg para testes
-	owner: '',
-	author: ''
+    neck: '',
+    material: '',
+    image: '', //https://http2.mlstatic.com/D_NQ_NP_902665-MLB31089875573_062019-O.jpg para testes
+    owner: '',
+    author: ''
 }
 let o1, o2, o3;
+let userName;
 
-function yourName (){
+function yourName() {
     let name = prompt("Por favor, digite seu nome: ");
+    userName = name;
     obj.author = name;
     obj.owner = name;
+    getShirtsFromServer();
 }
 
 window.onload = yourName;
 
+
 function changeBorderOption1(option1, type) {
     const typeSelected = document.querySelector(".options1 .borderColor");
-    if (typeSelected !== null){
+    if (typeSelected !== null) {
         typeSelected.classList.remove("borderColor")
     }
     option1.classList.add("borderColor");
@@ -31,7 +35,7 @@ function changeBorderOption1(option1, type) {
 
 function changeBorderOption2(option2, collar) {
     const collarSelected = document.querySelector(".options2 .borderColor");
-    if (collarSelected !== null){
+    if (collarSelected !== null) {
         collarSelected.classList.remove("borderColor")
     }
     option2.classList.add("borderColor");
@@ -42,7 +46,7 @@ function changeBorderOption2(option2, collar) {
 
 function changeBorderOption3(option3, material) {
     const materialSelected = document.querySelector(".options3 .borderColor");
-    if (materialSelected !== null){
+    if (materialSelected !== null) {
         materialSelected.classList.remove("borderColor")
     }
     option3.classList.add("borderColor");
@@ -55,14 +59,14 @@ function changeBorderOption3(option3, material) {
 
 //let img = document.querySelector("input").value;
 
-function turnOnButton (){
+function turnOnButton() {
     const buttonOn = document.getElementById("myBtn");
-    if ((o1 && o2  && o3 ) && validURL(document.getElementById("shirtIMG").value)){    // && validURL(document.getElementById("shirtIMG").value)
+    if ((o1 && o2 && o3) && validURL(document.getElementById("shirtIMG").value)) {    // && validURL(document.getElementById("shirtIMG").value)
         //console.log(validURL(document.getElementById("shirtIMG")));
         buttonOn.disabled = false;
         buttonOn.classList.add("finalizeOrder");
-        obj.image = document.querySelector("input").value;
-    } else{
+        obj.image = document.querySelector("input").value; //desabilitar essa linha para funcionar o post
+    } else {
         buttonOn.disabled = true;
         buttonOn.classList.remove("finalizeOrder");
     }
@@ -82,19 +86,19 @@ function sendOrder() {
     promise.catch(orderNotOk);
 }
 
-function orderOk (response) {
+function orderOk(response) {
     alert("Seu pedido foi confirmado! :)");
-    createHTML ();
+    createdOrderHTML();
     console.log(response);
 }
 
-function orderNotOk (response) {
+function orderNotOk(response) {
     alert("Ops, não conseguimos processar sua encomenda. :(");
     console.log(response);
     console.log(obj);
 }
 
-function createdOrderHTML (){
+function createdOrderHTML() {
     const lastOrders = document.querySelector(".containerFooter");
     lastOrders.innerHTML += "";
     lastOrders.innerHTML += `<div class="lastOrders">
@@ -103,3 +107,25 @@ function createdOrderHTML (){
 </div>
     `;
 }
+
+function getShirtsFromServer() {
+    const promise = axios.get(`${LINK_API}`);
+    promise.then(shirtsFromServer);
+    promise.catch();
+}
+
+function shirtsFromServer(answer) {
+    const lastOrdersFromServer = document.querySelector(".containerFooter");
+    lastOrdersFromServer.innerHTML += "";
+    for (i = 0; i < answer.data.length; i++) {
+        lastOrdersFromServer.innerHTML += `<div class="lastOrders">
+        <img src="${answer.data[i].image}}" alt="${answer.data[i].image}">
+        <p><strong>Criador: </strong>${answer.data[i].owner}</p>
+    </div>
+        `;
+    }
+}
+
+/* function buyLastOrder (){
+
+} */
